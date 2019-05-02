35399342a4c80324b35b417080da0c7a2894ad5d
alikygsz <46754653+alikygsz@users.noreply.github.com>
2019-05-02 15:11:45 +0300

    Update sayaç.py

--- sayaç.py
+++ sayaç.py
@@ -2,8 +2,6 @@ sayi = int(input("başlangıç sayısını giriniz:")) #burada kullanıcının s
 bitis = int (input("bitiş sayısını giriniz:")) #burada kullanıcının sayacı bitirmek istediği sayıyı istiyorsun
 artis = int (input("artış miktarını giriniz:")) #burada kullanıcının sayaca arttırmak istediği değeri istiyorsun
 
-while(True):#burada döngüyü başlatıyorsun true döngü tamamlanana kadar sonsuz işlem yap demek
-    print(sayi) #burada döngüde o an geçerli olan sayıyı yazdırıyorsun sayı arttıkça döngü tekrarlanıp büyüyen sayı yeniden yazılacak
-    sayi = sayi + artis #sayının döngü içinde artış miktarı kadar artmasını istediğini belirtiyorsun
-    if(sayi>=bitis): #son saayı bitiş sayısına ulaşırsa bitirmesini sağlayan komut
-        break #programı bitirir
+For i in range(sayi, bitis, artis) :
+    print(i) 
+
